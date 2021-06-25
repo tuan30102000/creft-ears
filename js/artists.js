37a2e7a8a03c92a621eba38981454d50336bd35b
tuan30102000 <vandathd49@gmail.com>
2021-06-26 03:44:23 +0700

    doneGetData

--- js/artists.js
+++ js/artists.js
@@ -1,19 +1,21 @@
-const artistsApp = {
-    statrs: function() {
-        this.handleAnimationtitle()
-    },
-    handleAnimationtitle: function() {
-        if ($(".artists-title__content").length == 1) {
-            var typed_strings = $(".artists-title__content").text();
-            var typed = new Typed(".artists-title__show", {
-                smartBackspace: !1,
-                strings: typed_strings.split(","),
-                typeSpeed: 80,
-                loop: true,
-                backDelay: 2e3,
-                backSpeed: 40,
-            })
-        }
-    },
-}
-artistsApp.statrs()
+(function(){
+    const artistsApp = {
+        statrs: function() {
+            this.handleAnimationtitle()
+        },
+        handleAnimationtitle: function() {
+            if ($(".artists-title__content").length == 1) {
+                var typed_strings = $(".artists-title__content").text();
+                var typed = new Typed(".artists-title__show", {
+                    smartBackspace: !1,
+                    strings: typed_strings.split(","),
+                    typeSpeed: 80,
+                    loop: true,
+                    backDelay: 2e3,
+                    backSpeed: 40,
+                })
+            }
+        },
+    }
+    artistsApp.statrs()
+})()
--- js/form.js
+++ js/form.js
@@ -1,17 +1,19 @@
-const formUi = {
-    statrs: function() {
-        this.handlEvent()
-    },
-    handlEvent: function() {
-        $('.form__input-elm').focus(
-            function(e) {
-                if (!e.target.value) e.target.parentElement.classList.add("active")
-            }
-        )
-        $('.form__input-elm').blur(
-            function(e) {
-                if (!e.target.value) e.target.parentElement.classList.remove("active")
-            })
-    },
-}
-formUi.statrs()
+(function(){
+    const formUi = {
+        statrs: function() {
+            this.handlEvent()
+        },
+        handlEvent: function() {
+            $('.form__input-elm').focus(
+                function(e) {
+                    if (!e.target.value) e.target.parentElement.classList.add("active")
+                }
+            )
+            $('.form__input-elm').blur(
+                function(e) {
+                    if (!e.target.value) e.target.parentElement.classList.remove("active")
+                })
+        },
+    }
+    formUi.statrs()
+})()
